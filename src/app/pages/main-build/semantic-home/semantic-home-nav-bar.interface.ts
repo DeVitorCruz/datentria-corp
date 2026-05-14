184d021b4e62557d09d4164718c00f580cf31f01
DeVitorCruz <devitorcruz@gmail.com>
2026-05-14 15:41:41 -0300

    refactor: move semantic-home to luxury theme main-build

--- src/app/pages/main-build/semantic-home/semantic-home-nav-bar.interface.ts
+++
@@ -1,5 +0,0 @@
-import { NavBarItem } from "../../../shared/ui/nav-bar/nav-bar-item.interface";
-
-export interface SemanticHomeNavBar {
-    navBar: NavBarItem;
-};
--- src/app/themes/luxury/theme.route.ts
+++ src/app/themes/luxury/theme.route.ts
@@ -4,7 +4,7 @@ import { Routes } from "@angular/router";
 export const JEWELRY_LUXURY_THEME_ROUTE: Routes = [
     {
         path: '',
-        loadComponent: () => import('../../pages/main-build/semantic-home/semantic-home.page')
+        loadComponent: () => import('./main-build/semantic-home/semantic-home.page')
 			.then(m => m.SemanticHome), 
         children: [
             {
