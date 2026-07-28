86696e7bf529818ee82473f46ded327c18c90deb
DeVitorCruz <devitorcruz@gmail.com>
2026-07-28 14:33:40 -0300

    feat(error-404): build 404 page with luxury styling
    - feat: Error404Page with large code, title, message and back to home button
    - style: error-404 luxury SCSS with Cinzel 404 code, tertiary color, CTA button
    - style: registered error-404 in pages collector

--- src/app/themes/luxury/pages/error-404/error-404.page.ts
+++ src/app/themes/luxury/pages/error-404/error-404.page.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-error-404',
-  imports: [],
+  imports: [ RouterLink ],
   templateUrl: './error-404.page.html',
   styleUrl: './error-404.page.scss'
 })
-export class Error404Page {
-
-}
+export class Error404Page {}
